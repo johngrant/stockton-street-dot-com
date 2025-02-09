15b30b038f53ad1e95f98c196dddce7be4e9d6af
John Grant <2171719+johngrant@users.noreply.github.com>
2025-02-09 17:35:57 -0500

    Update site metadta.

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://stockton-street-dot-com.vercel.app/',
+  siteRepo: 'https://github.com/johngrant/stockton-street-dot-com',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
